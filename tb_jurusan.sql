a6bd26e102dffffcc9e08351cb20148adda6cd26
andre effendi <81355291+andreeffendi@users.noreply.github.com>
2021-08-18 19:58:50 +0700

    menambahkan tabel SQL

---
+++ tb_jurusan.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Waktu pembuatan: 17 Agu 2021 pada 03.51
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_vision`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_jurusan`
+--
+
+CREATE TABLE `tb_jurusan` (
+  `id_jurusan` int(11) NOT NULL,
+  `nama_jurusan` varchar(5) NOT NULL,
+  `jumlah_anak` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_jurusan`
+--
+
+INSERT INTO `tb_jurusan` (`id_jurusan`, `nama_jurusan`, `jumlah_anak`) VALUES
+(1, 'RPL', 40),
+(2, 'MM', 50),
+(3, 'TKRO', 60),
+(4, 'AP', 46);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_jurusan`
+--
+ALTER TABLE `tb_jurusan`
+  ADD PRIMARY KEY (`id_jurusan`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_jurusan`
+--
+ALTER TABLE `tb_jurusan`
+  MODIFY `id_jurusan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
